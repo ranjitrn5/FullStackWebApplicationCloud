ebc6996de682e7204f2631561cb62d1472e2bcd8
ranjitrn5 <ranjitrn5@gmail.com>
2017-03-22 06:48:38 -0500

    Project Initial Commit

---
+++ springawsapplication/src/main/java/com/springawsapplication/SpringawsapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.springawsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawsapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringawsapplicationApplication.class, args);
+	}
+}

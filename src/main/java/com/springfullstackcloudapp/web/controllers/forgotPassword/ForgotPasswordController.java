a55fb2cd76a04c802fb4959fa666b7788429a36b
ranjitrn5 <ranjitrn5@gmail.com>
2017-06-16 19:05:57 -0500

    Implemented forgot password module and sending email verfication module

---
+++ src/main/java/com/springfullstackcloudapp/web/controllers/forgotPassword/ForgotPasswordController.java
@@ -0,0 +1,7 @@
+package com.springfullstackcloudapp.web.controllers.forgotPassword;
+
+/**
+ * Created by ranji on 6/16/2017.
+ */
+public class ForgotPasswordController {
+}
---
+++ src/test/java/com/springfullstackcloudapp/tests/unitTest/UserUtilsUnitTest.java
@@ -0,0 +1,7 @@
+package com.springfullstackcloudapp.tests.unitTest;
+
+/**
+ * Created by ranji on 6/16/2017.
+ */
+public class UserUtilsUnitTest {
+}
